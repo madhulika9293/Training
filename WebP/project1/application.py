313e90e12bf84bfbabbe4d386e44d178df0a3d08
madhulika9293 <ramyamadhulika9293@msitprogram.net>
2020-03-26 21:43:34 +0530

    task4-createdb

--- WebP/project1/application.py
+++ WebP/project1/application.py
@@ -1,25 +1,38 @@
 import os
+import datetime
 
 from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+	raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+	__tablename__ = "users"
+	fname = db.Column(db.String, nullable=False)
+	email = db.Column(db.String, nullable=False, primary_key=True)
+	password = db.Column(db.String, nullable=False)
+	timestamp = db.Column(db.DateTime, nullable=False)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
@@ -33,6 +46,7 @@ def register():
 		email = request.form.get("email")
 		password = request.form.get("password")
 		print(name + ", "+ email + ", " + password)
+		dt = datetime.datetime.now()
 		return render_template("index.html",name=name)
 	return render_template("registration.html")
 
